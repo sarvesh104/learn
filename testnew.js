22fc33e183bd24b90325076dca3f0ca17fdeb8f0
ayush5902 <mouryaayush191@gmail.com>
2023-05-08 12:44:45 +0700

    added name

--- testnew.js
+++ testnew.js
@@ -1,11 +1,5 @@
 console.log('hello world')
-<<<<<<< HEAD
 console.log('hi!!');
 console.log('ayush');
-=======
-<<<<<<< HEAD
 console.log('bhavin');
-=======
 console.log('hi!!');
->>>>>>> 77f72dbe655fc0398f63baae8573af87f4a8a6db
->>>>>>> c09775c100a412d9acb2d4926dedd027b630506f
