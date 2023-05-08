0346d9ca47307445a597cbd8341275a1d69c0db6
BhavinSapariya <sapariyabhavin012@gmail.com>
2023-05-08 11:33:43 +0530

    deleted hello wordl

--- testnew.js
+++ testnew.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-=======
-console.log('hello world')
->>>>>>> d9ad0c71fbeaa74cab6aabf01c5420bfe6cb2c01
 console.log('bhavin');
 console.log('hi!!');
 console.log('hi!!');
 console.log('ayush');
 console.log('bhavin');
 console.log('hi!!');
-<<<<<<< HEAD
-=======
 console.log(amrit);
->>>>>>> d9ad0c71fbeaa74cab6aabf01c5420bfe6cb2c01
