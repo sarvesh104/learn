6dd066d38d74889ebd595f6ef7b7c8d519c41f47
sarvesh104 <sarveshdusane104@gmail.com>
2023-05-08 11:19:13 +0530

    new

--- testnew.js
+++ testnew.js
@@ -1,10 +1,7 @@
 console.log('hello world')
-<<<<<<< HEAD
 console.log('bhavin');
 console.log('hi!!');
-=======
 console.log('hi!!');
 console.log('ayush');
 console.log('bhavin');
 console.log('hi!!');
->>>>>>> 22fc33e183bd24b90325076dca3f0ca17fdeb8f0
