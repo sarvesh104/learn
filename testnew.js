e96b499148a1b621d53d1325051e514b03363dab
sarvesh104 <sarveshdusane104@gmail.com>
2023-05-10 11:15:09 +0530

    new branch

--- testnew.js
+++ testnew.js
@@ -8,7 +8,6 @@ console.log('hi!!');
 console.log('ayush');
 console.log('bhavin');
 console.log('hi!!');
-<<<<<<< HEAD
-=======
 console.log(amrit);
->>>>>>> d9ad0c71fbeaa74cab6aabf01c5420bfe6cb2c01
+console.log(new branch);
+
